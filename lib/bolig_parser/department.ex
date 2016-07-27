cfaf37200ed5b7b4a2ae257bfb184501dda2eb8a
Oliver Theill Ryde <otr@keepfocus.dk>
2016-07-28 01:52:45 +0200

    Add address4

--- lib/bolig_parser/department.ex
+++ lib/bolig_parser/department.ex
@@ -1,4 +1,4 @@
 defmodule BoligParser.Department do
-  defstruct [:Address, :Address2, :Address3, :Address5, :City, :CompanyId, :Id, :LocalArea,
+  defstruct [:Address, :Address2, :Address3, :Address4, :Address5, :City, :CompanyId, :Id, :LocalArea,
  :Name, :PickerName, :YearOfBuild, :Zipcode, :DepartmentPropertiesdto, :TenancyDtos, :LocalDepartmentId]
 end
